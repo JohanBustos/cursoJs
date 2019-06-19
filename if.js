12960ecada4961766c57b0bde07f4305e97692b4
Johan Bustos <johan.bustosm@gmail.com>
2019-06-19 00:13:52 -0500

    primer if
    un if sencillo

---
+++ if.js
@@ -0,0 +1,9 @@
+var numberOne = 5
+var numberTwo = 4
+
+if (numberOne > numberTwo) { 
+    console.log("El numero es "+numberOne+" es mayor que el numero "+numberTwo)
+   console.log(`El numero ${numberOne} es mayor que el numero ${numberTwo}`)
+ }
+
+ 
